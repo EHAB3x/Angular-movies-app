656872eb618951be7efd04e3271dc94fe634c66f
EHAB3x <ehabmah6969@gmail.com>
2024-08-31 10:54:35 +0200

    finish home grid layout and add categories

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,18 +6,34 @@ import { HttpClientModule } from '@angular/common/http';
 import { Endpoints } from '../../endpoints/Endpoints';
 import { MovieCardConfig } from '../../interfaces/ui-config/movie-card-config.interface';
 import { TrendData, TrendsResult } from '../../interfaces/models/trends.interface';
+import { SegmentedControlConfig } from '../../interfaces/ui-config/segmented-control-config.interface';
+import { SegmentedControlComponent } from '../../components/segmented-control/segmented-control.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   providers:[GenericHttpService],
-  imports: [InputComponent, MovieCardComponent, HttpClientModule],
+  imports: [InputComponent, MovieCardComponent,SegmentedControlComponent, HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit{
   title : string = 'All';
   movieCards : MovieCardConfig[]=[];
+  segments: SegmentedControlConfig[] = [
+    {
+      name: 'All',
+      active: true,
+    },
+    {
+      name: 'Movies',
+      active: false,
+    },
+    {
+      name: 'TV Shows',
+      active: false,
+    },
+  ];
   constructor(
     private genericHttpService: GenericHttpService
   ){

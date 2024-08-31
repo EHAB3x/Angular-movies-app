a5d20dabdb3239b18524e230f56b13bfc7e67d1b
EHAB3x <ehabmah6969@gmail.com>
2024-08-31 08:38:24 +0200

    finish rate chip component

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,29 +1,33 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
-import { SegmentedControlComponent } from "../../components/segmented-control/segmented-control.component";
+import { SegmentedControlComponent } from '../../components/segmented-control/segmented-control.component';
 import { SegmentedControlConfig } from '../../interfaces/ui-config/segmented-control-config.interface';
-import { InputComponent } from "../../components/input/input.component";
+import { InputComponent } from '../../components/input/input.component';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [NavBarComponent, SegmentedControlComponent, InputComponent],
+  imports: [
+    NavBarComponent,
+    SegmentedControlComponent,
+    InputComponent,
+  ],
   templateUrl: './test.component.html',
-  styleUrl: './test.component.scss'
+  styleUrl: './test.component.scss',
 })
 export class TestComponent {
-  segments : SegmentedControlConfig[]=[
+  segments: SegmentedControlConfig[] = [
     {
-      name: "All",
+      name: 'All',
       active: true,
     },
     {
-      name:"Movies",
-      active: false
+      name: 'Movies',
+      active: false,
     },
     {
-      name:"TV Shows",
-      active: false
+      name: 'TV Shows',
+      active: false,
     },
-  ]
+  ];
 }

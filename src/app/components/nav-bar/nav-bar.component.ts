38905bf5f6f80a5c3e30044fb3f87a34f4b6ffdd
EHAB3x <ehabmah6969@gmail.com>
2024-08-31 07:45:00 +0200

    finish navbar component

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { NavItemConfig } from '../../interfaces/ui-config/nav-item-config.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  navItems: NavItemConfig[] = [
+    {
+      name: 'Movie',
+      path: 'movies',
+      active: false,
+    },
+    {
+      name:'TV Shows',
+      path:'tvshows',
+      active: false,
+    },
+    {
+      name:'Suggest me',
+      path:'suggests',
+      icon: 'bi bi-arrow-right',
+      active: false,
+    },
+  ];
 
+  selectedItem(nav : NavItemConfig){
+    this.navItems.map((item : NavItemConfig)=>{
+      item.active = nav.name === item.name
+    })
+  }
 }
--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
